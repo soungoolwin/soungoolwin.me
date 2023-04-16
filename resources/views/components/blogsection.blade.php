0fb56f8d714bf078fcf8fc38c576ab8283e32018
Soung Oo Lwin <soungoolwin275@gmail.com>
2023-04-16 23:39:59 +0700

    blogcard template fixed 5

--- resources/views/components/blogsection.blade.php
+++ resources/views/components/blogsection.blade.php
@@ -7,14 +7,14 @@
                 @foreach ($blogs as $blog)
                     <a href="/blogs/{{ $blog->slug }}">
                         <div class="blogsectioncard mx-auto text-center">
-                            <div class="relative pb-9/16">
+                            <div class="relative">
                                 <img src="{{ $blog->image_url }}" alt="" width="200" height="200"
                                     class="mx-auto pt-5" />
                             </div>
                             <div class="px-6
                                     py-4">
                                 <div class="font-bold text-xl mb-2">{{ $blog->title }}</div>
-                                <p class="text-gray-700 text-base">
+                                <p class="text-gray-700 text-base pb-10">
                                     {{ $blog->intro }}
                                 </p>
                             </div>
