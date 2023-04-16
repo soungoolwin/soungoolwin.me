02fa3205a1a2135e41b9de7cb1f83c94b75d3717
Soung Oo Lwin <soungoolwin275@gmail.com>
2023-04-16 23:16:31 +0700

    blog card template Fixed

--- resources/views/components/blogsection.blade.php
+++ resources/views/components/blogsection.blade.php
@@ -6,11 +6,13 @@
             <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 toflexblogsection justify-items-center">
                 @foreach ($blogs as $blog)
                     <a href="/blogs/{{ $blog->slug }}">
-                        <div class="blogsectioncard">
+                        <div class="blogsectioncard mx-auto text-center">
                             <div class="relative pb-9/16">
-                                <img src="{{ $blog->image_url }}" alt="" width="400" height="200" />
+                                <img src="{{ $blog->image_url }}" alt="" width="200" height="200"
+                                    class="mx-auto my-5" />
                             </div>
-                            <div class="px-6 py-4">
+                            <div class="px-6
+                                    py-4">
                                 <div class="font-bold text-xl mb-2">{{ $blog->title }}</div>
                                 <p class="text-gray-700 text-base">
                                     {{ $blog->intro }}
--- resources/views/mail/accverification.blade.php
+++ resources/views/mail/accverification.blade.php
@@ -10,7 +10,6 @@
     <p>Please click on the link below to verify your email addressss:</p>
 
     <a href="{{ url('verify-email/' . $user->email_verification_token) }}">Verify Email Address</a>
-
 </body>
 
 </html>
