9fc9566a161430f9942438fab3c6e7c6e54d5b10
Soung Oo Lwin <soungoolwin275@gmail.com>
2023-06-08 13:17:45 +0700

    Error Alert Via Discord

---
+++ app/Console/Commands/CheckWebsiteStatus.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Blog;
+use Illuminate\Console\Command;
+use App\Services\DiscordNotificationService;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
+class CheckWebsiteStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'website:status-check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check website status and send notifications';
+    protected $discordNotificationService;
+
+    /**
+     * Execute the console command.
+     */
+    public function __construct(DiscordNotificationService $discordNotificationService)
+    {
+        parent::__construct();
+        $this->discordNotificationService = $discordNotificationService;
+    }
+
+    public function handle()
+    {
+
+
+        $urlsToCheck = [
+
+            'https://soungoolwin.me/',
+            'https://soungoolwin.me/login/',
+            'https://soungoolwin.me/signup/',
+            'https://soungoolwin.me/signup/verify',
+            'https://soungoolwin.me/dashboard/blogs/create',
+            'https://soungoolwin.me/dashboard/blogs/showall',
+        ];
+        $client = new Client();
+
+        foreach ($urlsToCheck as $url) {
+            try {
+                $response = $client->get($url);
+                $statusCode = $response->getStatusCode();
+
+                if ($statusCode === 200) {
+                    $this->info("URL: $url - Status: $statusCode");
+                } else {
+                    $this->error("URL: $url - Status: $statusCode");
+                    $this->discordNotificationService->sendErrorNotification("Website Have an errors");
+                }
+            } catch (ClientException $exception) {
+                $response = $exception->getResponse();
+                $statusCode = $response->getStatusCode();
+                $this->error("URL: $url - Status: $statusCode");
+
+                // Handle the 404 error here, such as sending a Discord notification
+                $this->discordNotificationService->sendErrorNotification("URL: $url - Status: $statusCode");
+            }
+        }
+
+        $this->info('Website status check completed.');
+    }
+}
--- app/Services/DiscordNotificationService.php
+++ app/Services/DiscordNotificationService.php
@@ -18,11 +18,22 @@ class DiscordNotificationService
 
 
         $payload = [
-            'content' => "**User Signup Event**\n\nNew user signed up on your website!\n\nDetails:\n" . json_encode($userData['email']),
+            'content' => "**User Signup Event**\n\nNew user Logged in on your website!\n\nDetails:\n" . json_encode($userData['email']),
         ];
 
         $client = new Client();
         $client->post($this->webhookUrl, ['json' => $payload]);
+    }
+    public function sendErrorNotification($message)
+    {
+        $payload = [
+            'content' => 'Error: ' . $message,
+        ];
+
+        $client = new Client();
+        $client->post($this->webhookUrl, [
+            'json' => $payload,
+        ]);
     }
 
     // Implement other notification methods as needed
